dbf9900233bf53964db930f2c6b8448f79fc9f7b
greig hearne <greig.hearne@soprabanking.com>
2019-09-24 16:00:43 +0100

    COMMIT

--- Lectures_6_to_999/app.js
+++ Lectures_6_to_999/app.js
@@ -1,4 +1,27 @@
 "use strict";
 /**/
 console.log('It works..!');
-/**/ 
+/**/
+/**/
+/* DON'T FORGET TO RUN npm start FOR THIS PROJECT */
+/**/
+/* STRINGS.....
+                        let myName = 'Greig';
+                        /* Then tried to do the following, we'd get a typescript error when we run 'tsc' at a terminal prompt)
+                        myName = 47;
+                        /* like this  :-
+                        /*
+                        PS C:\grh\Courses\TypeScript-Course-1\Lectures_6_to_999> tsc
+                        app.ts:10:1 - error TS2322: Type '47' is not assignable to type 'string'.
+
+                        10 myName = 47;
+                        ~~~~~~
+                        Found 1 error.
+*/
+/* NUMBERS.....   */
+var myAge = 47;
+myAge = 'Greig';
+/**/
+/**/
+/**/
+/**/
--- Lectures_6_to_999/app.ts
+++ Lectures_6_to_999/app.ts
@@ -1,3 +1,34 @@
 /**/
 console.log('It works..!');
 /**/
+/**/
+/* DON'T FORGET TO RUN npm start FOR THIS PROJECT */
+/**/
+/* STRINGS.....
+                        let myName = 'Greig';
+                        /* Then tried to do the following, we'd get a typescript error when we run 'tsc' at a terminal prompt) 
+                        myName = 47;
+                        /* like this  :- 
+                        /*
+                        PS C:\grh\Courses\TypeScript-Course-1\Lectures_6_to_999> tsc
+                        app.ts:10:1 - error TS2322: Type '47' is not assignable to type 'string'.
+
+                        10 myName = 47;
+                        ~~~~~~
+                        Found 1 error.
+*/
+/* NUMBERS.....  
+                        let myAge = 47;
+                        /* Then tried to do the following, we'd get a typescript error when we run 'tsc' at a terminal prompt)
+                        myAge = 'Greig';
+                        /*
+                        PS C:\grh\Courses\TypeScript-Course-1\Lectures_6_to_999> tsc
+                        app.ts:22:1 - error TS2322: Type '"Greig"' is not assignable to type 'number'.
+
+                        22 myAge = 'Greig';
+                        ~~~~~
+                        Found 1 error.
+/**/
+/**/
+/**/
+/**/
