942513d834b119cb3bd74f7efdadd89c4764a1a6
greig hearne <greig.hearne@soprabanking.com>
2020-01-13 17:21:37 +0000

    commit_2

--- Lecture_2_TS_Basics _and_Basic_Types/app.js
+++ Lecture_2_TS_Basics _and_Basic_Types/app.js
@@ -1,2 +1,14 @@
 //
-console.log('hello world....your code goes here');
+/*  To get his working we need a lite-server running so :-
+
+    1. run "npm init"
+    2. run "npm install --save-dev lite-server"
+    3. in package.json add the following after "test", which is in "scripts"    -->    "start": "lite-server"
+    4. run "npm install"
+    5. run "tsc --init"
+    6. nmp start (the name of the script in "package.json")
+    7. tsc (in a NEW terminal window)
+    8. F12 in the browser to see the console messages
+*/
+//
+console.log('hello world....boom !!!');
--- Lecture_2_TS_Basics _and_Basic_Types/app.ts
+++ Lecture_2_TS_Basics _and_Basic_Types/app.ts
@@ -3,14 +3,15 @@
 
     1. run "npm init"
     2. run "npm install --save-dev lite-server"
-    2. run "npm install"
-    3. run "tsc --init"
-    4. nmp start (the name of the script in "package.json")
-    5. tsc (in a NEW terminal window)
-    6. F12 in the browser to see the console messages
+    3. in package.json add the following after "test", which is in "scripts"    -->    "start": "lite-server"
+    4. run "npm install"
+    5. run "tsc --init"
+    6. nmp start (the name of the script in "package.json")
+    7. tsc (in a NEW terminal window)
+    8. F12 in the browser to see the console messages
 */
 //
 
-console.log('hello world....');
+console.log('hello world....boom !!!');
 
 
